89fceb790bd8a5e84a680befff0af4d693d090f9
Balram Panda <97830953+Balram72@users.noreply.github.com>
2024-12-25 11:00:31 +0000

    string literals

---
+++ string literals.js
@@ -0,0 +1,9 @@
+ // string literals
+ let names = 'balram';
+ let score = 100;
+ //old concept
+ console.log(names + 'score' + score + 'run today <br/>');
+  // string literals
+ console.log(`name ${names} score ${score} run today`);
+
+ 
