1a1f15ae82ea827065dcc5e5d7d012ec3f35f3a1
Balram Panda <97830953+Balram72@users.noreply.github.com>
2024-12-25 11:11:21 +0000

    argument keyWord

---  argument keyWord.js
+++  argument keyWord.js
@@ -27,3 +27,19 @@ function add() {
   student('Balram', 90)
   student('sunita', 90, 90, 90)
   student('ram', 90, 90)
+
+//argument keyWord example
+//************************
+  function add1() {
+    let sum = 0;
+    for (i of arguments) {
+      sum += i;
+    }
+    console.log(`${sum} <br/>`);
+  }
+  add1(10);
+  add1(10, 20);
+  add1(10, 20, 30);
+  add1(10, 20, 30, 40);
+  add1(10, 20, 30, 40, 50);
+  // output : -10,30,60,100,150
